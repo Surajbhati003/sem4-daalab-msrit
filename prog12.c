c842181707919e9a41836b61f3a8fe4ffb67b8dd
Surajbhati003 <surajbhati003@gmail.com>
2024-09-01 15:14:04 +0530

    Update prog12.c

--- prog12.c
+++ prog12.c
@@ -6,84 +6,59 @@ taken. Specify the time efficiency class of this algorithm.
 */
 // Heap Sort in C
 
-#include <stdio.h>
-void swap(int* a, int* b)
+#include<stdio.h>
+
+void insert(int a[],int n)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void heapify(int arr[], int N, int i)
-{
-    // Find largest among root,
-    // left child and right child
-
-    // Initialize largest as root
-    int largest = i;
-
-    int left = 2 * i + 1;
-
-    int right = 2 * i + 2;
-
-    // If left child is larger than root
-    if (left < N && arr[left] > arr[largest])
-        largest = left;
-
-    // If right child is larger than largest
-    if (right < N && arr[right] > arr[largest])
-        largest = right;
-
-    // Swap and continue heapifying
-    // if root is not largest
-    // If largest is not root
-    if (largest != i) 
+    int i=n,temp;
+    temp=a[i];
+    while(i>1 && temp>a[i/2])
     {
-        swap(&arr[i], &arr[largest]);
-
-        // Recursively heapify the affected
-        // sub-tree
-        heapify(arr, N, largest);
+        a[i]=a[i/2];
+        i=i/2;
     }
+    a[i]=temp;
 }
 
-// Main function to do heap sort
-void heapSort(int arr[], int N)
+int delete(int a[],int n)
 {
-
-    // Build max heap
-    for (int i = N / 2 - 1; i >= 0; i--)
-
-        heapify(arr, N, i);
-
-    // Heap sort
-    for (int i = N - 1; i >= 0; i--) {
-
-        swap(&arr[0], &arr[i]);
-
-        // Heapify root element
-        // to get highest element at
-        // root again
-        heapify(arr, i, 0);
+    int i,j,x,temp,val;
+    val=a[1];
+    x=a[n];
+    a[1]=a[n];
+    a[n]=val;
+    i=1;j=i*2;
+    while(j<=n-1)
+    {
+        if(j<n-1 && a[j+1]>a[j])
+            j=j+1;
+        if(a[i]<a[j])
+        {
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+            i=j;
+            j=2*j;
+        }
+        else
+            break;
     }
+    return val;
 }
 
-// A utility function to print array of size n
-void printArray(int arr[], int N)
+void main()
 {
-    for (int i = 0; i < N; i++)
-        printf("%d ", arr[i]);
+    int a[]={0, 14, 15, 5, 20, 30, 8, 40};
+    int i;
+    for(i=2;i<=7;i++)
+        insert(a,i);
+
+    for(i=7;i>1;i--)
+        delete(a,i);
+
+    for(i=1;i<=7;i++)
+        printf("%d ",a[i]);
     printf("\n");
-}
 
-// Driver's code
-int main()
-{
-    int arr[] = { 12, 11, 13, 5, 6, 7 };
-    int N = sizeof(arr) / sizeof(arr[0]);
-
-    // Function call
-    heapSort(arr, N);
-    printf("Sorted array is\n");
-    printArray(arr, N);
+    return 0;
 }
